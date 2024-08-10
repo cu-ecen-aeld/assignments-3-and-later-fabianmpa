f99ec9fd336601fb122f111ab2889d6d0acf6ea3
Fabian Manuel Pichardo <fabianmpa019@gmail.com>
2024-08-09 23:31:18 -0700

    Add steps to compile kernel, copy shared libraries and create the initramfs.cpio.gz file Still need to cross compile writer and finder files

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -22,6 +22,10 @@ else
 fi
 
 mkdir -p ${OUTDIR}
+if [ ! -d ${OUTDIR} ]
+then
+    exit 1
+fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -33,8 +37,12 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all 
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs 
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +56,12 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -60,15 +74,30 @@ else
     cd busybox
 fi
 
+echo "Building and installing busybox"
 # TODO: Make and install busybox
+make distclean 
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+PROGINT=$( find -L ${HOME} -type f -name ld-linux-aarch64.so.1 )
+cp ${PROGINT} ${OUTDIR}/rootfs/lib
+LIBM=$( find -L ${HOME} -type f -name libm.so.6 )
+cp ${LIBM} ${OUTDIR}/rootfs/lib64
+LIBRESOLV=$( find -L ${HOME} -type f -name libresolv.so.6 )
+cp ${LIBRESOLV} ${OUTDIR}/rootfs/lib64
+LIBC=$( find -L ${HOME} -type f -name libc.so.6 )
+cp ${LIBC} ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
 
@@ -78,3 +107,8 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 # TODO: Chown the root directory
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f initramfs.cpio
+
+.${HOME}/linuxprogramming/assignment-1-fabianmpa/finder-app/start-qemu-terminal.sh
