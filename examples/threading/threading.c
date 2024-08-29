d8d146af512ae0aa1fbf48b2223325342a7c0bcf
Fabian Manuel Pichardo <fabianmpa019@gmail.com>
2024-08-28 23:43:32 -0700

    Assignment 4 Part 1 Completed

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,7 +15,15 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* thread_args = (struct thread_data*)thread_param;
+    unsigned int usec =  (thread_args->wait_to_obtain_ms)*1000;
+    usleep(usec);
+    pthread_mutex_lock(thread_args->mutex);
+    usec =  (thread_args->wait_to_release_ms)*1000;
+    usleep(usec);
+    pthread_mutex_unlock(thread_args->mutex);
+    thread_args->thread_complete_success = true;
+    return thread_args;
 }
 
 
@@ -28,6 +37,31 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    if(thread != NULL){   
+        int ret;
+        struct thread_data* data_params = (struct thread_data*) malloc(sizeof(struct thread_data));
+        if (data_params == NULL)
+        {
+            perror("mem allocation failed");
+            return false;
+        }
+        else{
+            data_params->mutex = mutex;
+            data_params->wait_to_obtain_ms = wait_to_obtain_ms;
+            data_params->wait_to_release_ms = wait_to_release_ms;
+            ret = pthread_create(thread,NULL,threadfunc,(void *)data_params);
+            if(ret!=0){
+                errno = ret;
+                perror("thread was not created");
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+    else{
+        return false;
+    }
 }
 
