8a1508adf93d7995670f2a9cf78c974fa3f58715
Fabian Pichardo <fabianmpa019@gmail.com>
2025-05-03 22:12:41 -0600

    -Task solved with new implementation:     - Use one fd for write and other for read.     - Implement function to know if stream has ended

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,17 +12,20 @@
 #include <signal.h>
 #include <libexplain/bind.h>
 
-#define buffersize 0x4FFFU
-int sockfd, clnt_cn, fileDescriptor, pid;
+#define BUF_SIZE 1024
+int sockfd, clnt_cn, fileDescriptor, pid, write_descriptor, read_descriptor;
 int char_count = 0U;
+int ret_bytes = 0U;
+int packet_end = 0;
 struct in_addr address;
 struct sockaddr_in serv_addr, client_addr;
 socklen_t client_sock_size = sizeof(client_addr);
-ssize_t mlength = 0;
+ssize_t clnt_read_char = 0;
+ssize_t clnt_ret_char = 0;
 char* recv_buffer = NULL;
 char* ret_buff = NULL;
-int isDeamon = 0U;;
-
+int isDeamon = 0U;
+int hasNewLine(char* string_stream, int size);
 void sig_handler(int signo){
     if(signo == SIGINT){
         syslog(LOG_DEBUG,"Caught SIGINT signal! \n");
@@ -36,8 +39,9 @@ void sig_handler(int signo){
     remove("/var/tmp/aesdsocketdata.txt");
     free(recv_buffer);
     free(ret_buff);
-    close(fileDescriptor);
     close(sockfd);
+    close(write_descriptor);
+    close(read_descriptor);
     close(clnt_cn);
     closelog();
     exit (EXIT_SUCCESS);
@@ -46,8 +50,10 @@ void sig_handler(int signo){
 void fault_handler(){
     free(recv_buffer);
     free(ret_buff);
-    close(fileDescriptor);
+    close(write_descriptor);
+    close(read_descriptor);
     close(sockfd);
+    close(clnt_cn);
     closelog();
     exit(EXIT_FAILURE);
 }
@@ -118,42 +124,66 @@ int main(int argc, char** argv)
         fault_handler();
     }
 
-    recv_buffer   = (char*) malloc(buffersize);
-    ret_buff      = (char*) malloc(buffersize);
+    recv_buffer   = (char*) malloc(BUF_SIZE);
+    ret_buff      = (char*) malloc(BUF_SIZE);
 
     if( (recv_buffer == NULL) | (ret_buff == NULL)){
         syslog(LOG_DEBUG, "Not enough heap memory");
         fault_handler();
     }
 
+    close(fileDescriptor);
+    int write_descriptor = open("/var/tmp/aesdsocketdata.txt", O_WRONLY | O_APPEND);
+    int read_descriptor = open("/var/tmp/aesdsocketdata.txt", O_RDONLY);
+
     while( 1 ) {
         int ret;
         clnt_cn = accept(sockfd, (struct sockaddr *) &client_addr, &client_sock_size);
         if(clnt_cn > 0){
             syslog(LOG_DEBUG,"Accepted connection from %s",inet_ntoa(client_addr.sin_addr));
-            mlength = read(clnt_cn, recv_buffer, buffersize);;
-            char_count = char_count + mlength;
-            if( mlength > 0 ){
-                ret = write(fileDescriptor,recv_buffer,mlength);
-                close(fileDescriptor);
-                fileDescriptor = open("/var/tmp/aesdsocketdata.txt", O_RDWR);
-                ret = pread(fileDescriptor, ret_buff, char_count,0);
-                ret = write(clnt_cn, ret_buff, char_count);
-                close(fileDescriptor);
-                fileDescriptor = open("/var/tmp/aesdsocketdata.txt", O_WRONLY | O_APPEND);
-                close(clnt_cn);
-                syslog(LOG_DEBUG,"Closed connection from %s",inet_ntoa(client_addr.sin_addr));
-                if(ret){;}
+            while((clnt_read_char = read(clnt_cn, recv_buffer, BUF_SIZE)) > 0 ){
+                packet_end = hasNewLine(recv_buffer,clnt_read_char);
+                char_count += clnt_read_char;
+                ret = write(write_descriptor,recv_buffer,clnt_read_char);
+                if(ret == -1){
+                    syslog(LOG_DEBUG, "Write error");
+                    fault_handler();
+                }
+                if(packet_end){
+                    if(char_count > BUF_SIZE){
+                        while((ret_bytes = read(read_descriptor, ret_buff, BUF_SIZE)) > 0 ){
+                            ret = write(clnt_cn, ret_buff, ret_bytes);
+                            if(ret == -1){
+                                syslog(LOG_DEBUG, "Write error");
+                                fault_handler();
+                            }
+                        }
+                    }else{
+                        int ret_bytes = pread(read_descriptor, ret_buff, BUF_SIZE,0);
+                        ret = write(clnt_cn, ret_buff, ret_bytes);
+                        if(ret == -1){
+                        syslog(LOG_DEBUG, "Write error");
+                        fault_handler();
+                        }
+                    }
+                }
             }
-            else{
-                close(clnt_cn);
-                break;
-            }
+            close(clnt_cn);
+            syslog(LOG_DEBUG,"Closed connection from %s",inet_ntoa(client_addr.sin_addr));
         }
         else{
             syslog(LOG_DEBUG, "Connection not accepted");
             close(clnt_cn);
             break;
+        }
+    }
+    return 0;
+}
+
+int hasNewLine(char* string_stream, int size){
+    for(int letter = 0; letter< size ; letter++){
+        if((*(string_stream+letter)) == '\n'){
+            return 1;
         }
     }
     return 0;
